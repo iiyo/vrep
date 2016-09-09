c0e55e7b8642682448574874e38986b07933cecc
Jonathan Steinbeck <jonathan@steinbeck.in>
2016-09-09 21:46:03 +0200

    Add an 'escape' function parameter to 'create', update README accordingly

--- README.md
+++ README.md
@@ -15,10 +15,10 @@ var text1 = format("Hi, my name is {name} and I'm {age}.", {name: "Marc", age: "
 var text2 = format("Hi, my name is {1} and I'm {2}.", ["Marc", "23"]);
 ```
 
-Use your own variable style:
+Use your own variable style and encode the values before insertion:
 
 ```javascript
-var format = require("vrep").create("[$", "]");
+var format = require("vrep").create("[$", "]", encodeURIComponent);
 
 var text1 = format("Hi, my name is [$name] and I'm [$age].", {name: "Marc", age: "23"});
 var text2 = format("Hi, my name is [$1] and I'm [$2].", ["Marc", "23"]);
--- vrep.js
+++ vrep.js
@@ -4,25 +4,34 @@
     
     var format = create("{", "}"), output;
     
-    function create (left, right) {
+    function id (v) {
+        return v;
+    }
+    
+//
+// Creates a format function using `left` as the left delimiter, `right` as the right
+// delimiter. If `filter` is supplied, every value inserted into the input template is run
+// through this `filter` function. This can be used to add HTML escaping and similar things.
+//
+    function create (left, right, escape) {
         
         if (typeof left !== "string" || typeof right !== "string") {
             throw new Error("Arguments left and right must be strings.");
         }
         
+        escape = (typeof escape === "function" ? escape : id);
+        
         return function (text, values) {
-            
-            var key;
             
             if (Array.isArray(values)) {
                 values.forEach(function (value, i) {
-                    text = text.split(left + (i + 1) + right).join(value);
+                    text = text.split(left + (i + 1) + right).join(escape(value));
                 });
             }
             else {
-                for (key in values) {
-                    text = text.split(left + key + right).join(values[key]);
-                }
+                Object.keys(values).forEach(function (key) {
+                    text = text.split(left + key + right).join(escape(values[key]));
+                });
             }
             
             return text;
